6826f9cb4050279313fafdd8416329015be73f02
afiqzudinhadi <afiqzudinhadi@gmail.com>
2026-06-03 23:09:39 +0800

    chore: disable Console to free a USB endpoint for raw HID

--- keyboards/noll/ecosteno/rules.mk
+++ keyboards/noll/ecosteno/rules.mk
@@ -10,7 +10,7 @@ SRC =	keyboard.c
 BOOTMAGIC_ENABLE = no	# Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no	# Mouse keys
 EXTRAKEY_ENABLE = no	# Audio control and System control
-CONSOLE_ENABLE = yes	# Console for debug
+CONSOLE_ENABLE = no		# Console for debug
 COMMAND_ENABLE = yes    # Commands for debug and configuration
 SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes	    # USB Nkey Rollover
@@ -18,6 +18,5 @@ COMBO_ENABLE = yes		# Enable Key Combos
 STENO_ENABLE = yes		# Stenography keys
 STENO_PROTOCOL = geminipr # Can be geminipr or txbolt
 MOUSEKEY_ENABLE = no	# Mouse keys
-
-RAW_ENABLE = yes
+RAW_ENABLE = yes		# RAW HID
 
